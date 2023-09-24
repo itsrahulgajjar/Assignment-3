f0da5b6ce49183cc8318e96a1ee1ca03c25d6745
Rahul Gajjar <rahulgajjar23@gmail.com>
2023-09-24 16:28:34 +0530

    add commit 1

--- todolist.py
+++ todolist.py
@@ -4,7 +4,7 @@ tasks = []
 
 def add_task(task, priority):
     tasks.append({"task": task, "priority": priority})
-
+#for commit 1
 def view_tasks():
     for i, task in enumerate(tasks, start=1):
         print(f"{i}. {task}")
